fa5d871c67a0f04364bb023c549897b132322f53
Rhenan Bartels <rhenan.bartels@gmail.com>
2020-08-07 15:35:03 -0300

    Add smoke tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,39 @@
 import { expect } from 'chai';
+import { search, searchAlbums, searchArtists, searchTracks, searchPlaylists } from '../src/main';
 
-describe('Main', () => {
+describe('Spotify Wrapper', () => {
+
+    describe('Smoke tests', () => {
+
+        it('should exist the search method', () => {
+            expect(search).to.exist;
+
+        });
+
+        it('should exist the searchAlbums methos', () => {
+            expect(searchAlbums).to.exist;
+        });
+
+        it('should exist the searchAlgumns methos', () => {
+            expect(searchArtists).to.exist;
+        });
+
+        it('should exist the searchAlgumns methos', () => {
+            expect(searchTracks).to.exist;
+        });
+
+        it('should exist the searchAlgumns methos', () => {
+            expect(searchPlaylists).to.exist;
+        });
+    });
+
+    describe('Generic Search', () => {
+
+        it('shoud call fetch function', () => {
+            const artists = search();
+
+        });
+    });
+
 
 });
